b9bf5b9e235294f079af366851516c586cd9d97a
SamaraCardoso27 <samaracardosodossantos@gmail.com>
2015-07-25 15:22:28 -0300

    inserção com webservice

--- controllers/webservice.py
+++ controllers/webservice.py
@@ -5,35 +5,39 @@ from datetime import datetime
 
 service = Service(globals())
 
-@service.soap('getKeyPointPerson',returns={'result':str},
+@service.soap('getKeyPointPerson',returns={'result':str,'code':int},
                             args={'auth':str,'person_data' : {'full_name':str, 'email':str, 'cellphone':str,
                                                           'birth_date':str,'keypoints': str}})
 def getKeyPointPerson(auth,person_data):
     print('passou aqui')
     #person_data['birth_date'] = datetime.strptime(person_data['birth_date'], "%d/%m/%Y").strftime("%m-%d-%Y")
-    #if auth == '' or auth == None or person_data == None or person_data == '':
-    #    return dict(result='NOT_VALID',code=0)
-    #else:
-    #    ret = db.executesql("""SELECT auth_user.id AS user_id
-    #                            FROM auth_user, company
-    #                            WHERE last_name='{0}' AND 
-    #                                  auth_user.actived_user= 'T' AND 
-    #                                  first_name='webservice' LIMIT 1
-    #                           """.format(auth),as_dict=True)
-    #    if len(ret) <= 0:
-    #        return dict(result='ERRO',code=0)
-    #    else:
-    #        id_person = db.executesql("""INSERT INTO person (created, last_update, last_user, full_name, email, cellphone, birth_date, keypoints)
-    #                                    VALUES ('{0}', '{0}', {1}, '{2}', '{3}', '{4}', '{5}','{5}')RETURNING id;;
-    #                                    """.format(datetime.now(),ret[0]['user_id'],person_data['full_name'],person_data['email'],
-    #                                               person_data['cellphone'],person_data['birth_date'],
-    #                                               person_data['keypoints']))
+    if auth == '' or auth == None or person_data == None or person_data == '':
+        return dict(result='NOT_VALID',code=0)
+    else:
+        ret = db.executesql("""SELECT auth_user.id AS user_id
+                                FROM auth_user, company
+                                WHERE last_name='{0}' AND 
+                                      auth_user.actived_user= 'T' AND 
+                                      first_name='webservice' LIMIT 1
+                               """.format(auth),as_dict=True)
+        if len(ret) <= 0:
+            return dict(result='ERRO',code=0)
+        else:
+            id_person = db.executesql(""" INSERT INTO person(created, last_update, full_name, email, cellphone, birth_date, keypoints, last_user)
+                                          VALUES ('{0}','{0}', '{1}','{2}','{3}','{4}','{5}',{6}) RETURNING id;
+                                        """.format(datetime.now(),person_data['full_name'],person_data['email'],
+                                                   person_data['cellphone'],person_data['birth_date'],
+                                                   person_data['keypoints'],ret[0]['user_id']))
+            if len(id_person) >= 1:
+                return dict(result='OK',code=1)
+            else:
+                return dict(result='NOT_VALID',code=0)
+                
+    #print(person_data['keypoints'])
     
-    print(person_data['keypoints'])
     
-    
-    return dict(result=person_data['keypoints'])
-    #return dict(result='NOT_VALID',code=0)
+            
+    #
 
 def call():
     return service()
--- models/db.py
+++ models/db.py
@@ -18,7 +18,7 @@ myconf = AppConfig(reload=True)
 
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
+    #db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
     db = DAL('postgres://postgres:q1w2E#R$@188.166.24.73/TG_Samara',pool_size=1,check_reserved=['all'])
 else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
